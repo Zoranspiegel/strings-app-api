3a1e1f55bdba4e34c6c02a11afd3dc0da17aa3d0
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-05 20:34:10 -0500

    pg migration

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,18 @@
 import { Client } from 'pg';
 
 export function getClient (): Client {
-  const client = new Client({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    port: Number(process.env.PG_PORT)
-  });
+  if (process.env.PG_RAILWAY_DB) {
+    const client = new Client(process.env.PG_RAILWAY_DB);
+    return client;
+  } else {
+    const client = new Client({
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DB,
+      port: Number(process.env.PG_PORT)
+    });
 
-  return client;
+    return client;
+  }
 }
