1aced1a83d133841ec6f0913697e2b8f33aedf7f
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-08 03:46:53 -0500

    new build

--- build/src/app.js
+++ build/src/app.js
@@ -8,10 +8,12 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const routes_1 = __importDefault(require("./routes"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: '*',
+    origin: process.env.FRONT_ORIGIN,
     credentials: true
 }));
 app.use((0, cookie_parser_1.default)());
