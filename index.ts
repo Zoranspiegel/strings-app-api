5a397ea7e46a9cdbc1ca1b7ddbcf92caac478329
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-08 03:46:19 -0500

    dotenv config fixed

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import server from './src/app';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const PORT = process.env.PORT ?? 3001;
 
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
