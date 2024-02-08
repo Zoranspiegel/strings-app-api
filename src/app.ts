27a0c10428581319c0ab40ad47b785f18f3fcaac
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-08 03:38:29 -0500

    front-end cors origin

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: process.env.FRONT_ORIGIN,
   credentials: true
 }));
 app.use(cookieParser());
